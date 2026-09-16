18858cae2a74dc2718cc9ea12904f8e7c27d90bb
Akash Rahangdale <akash82in@gmail.com>
2026-09-16 10:46:17 +0000

    Added cloudwatch log group

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,3 +1,9 @@
+
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name              = "/aws/lambda/jenkins-terraform-hello-world"
+  retention_in_days = 14 
+}
+
 resource "aws_lambda_function" "hello_world" {
   function_name = "jenkins-terraform-hello-world"
   role          = aws_iam_role.lambda_role.arn
@@ -6,6 +12,16 @@ resource "aws_lambda_function" "hello_world" {
   handler = "app.lambda_handler_v1"
 
   filename = "${path.module}/lambda_function.zip"
+  logging_config {
+    log_format = "Text" # Or "JSON"
+    log_group  = aws_cloudwatch_log_group.lambda_logs.name
+  }
+
+  # Ensure the log group is built before the Lambda executes to prevent auto-creation conflicts
+  depends_on = [
+    aws_cloudwatch_log_group.lambda_logs,
+    aws_iam_role_policy_attachment.lambda_logs,
+	]
 
   timeout = 10
 }
